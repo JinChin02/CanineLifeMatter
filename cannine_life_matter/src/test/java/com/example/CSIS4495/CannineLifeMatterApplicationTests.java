49224413a0e622f1173d03aace2c433ec0381343
LiangkuLin <jerry031785@gmail.com>
2022-05-28 11:37:57 -0700

    Initial Commit

---
+++ cannine_life_matter/src/test/java/com/example/CSIS4495/CannineLifeMatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CSIS4495;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CannineLifeMatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
