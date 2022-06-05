e5c85c2b20bf44c8e6e366ba1d259532a379badb
Jin Chin <chinj7@student.douglascollege.ca>
2022-06-04 17:45:44 -0700

    Added login form and connected with springboot backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
-import WithNavigation from './components/WithNavigation';
+import Login from './components/Login';
 
 function App() {
   
   return (
     <div className="App">
       <Header/>
+      <Login/>
       <Footer/>
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,71 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class Login extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            username:'',
+            password:'',
+            signinSuccess: false
+        }
+    }
+
+    userLogin = () => {
+        console.log(this.state.username);
+        console.log(this.state.password);
+
+        var user = {"username":this.state.username, "password":this.state.password}
+        axios.post("http://localhost:8080/login",user)
+        .then(response => this.abc(response.status)) 
+        
+    }   
+
+     abc = (res) => {
+        if(res === 200){
+            console.log(res)
+            this.setState({signinSuccess: true},() => console.log(this.state.signinSuccess))
+        }
+        
+    }
+
     render(){
-        return(
-            <></>
+        return (
+            <div>
+                <div className="container">
+                <h3 className="loginTitle">SIGN IN TO CONTINUE</h3><hr />
+                <form action="" >
+                    <label>Username</label><br />   
+                    <input type="text" name="username" onChange={evt => this.setState({username: evt.target.value})}/>
+                    <br />
+                    <label>Password</label><br />
+                    <input type="password" name="password" onChange={evt => this.setState({password: evt.target.value})}/>
+                    <br /><br />
+                    <button type="button" className="btn btn-success" onClick={this.userLogin}>Login</button>
+
+                </form>
+                    {/* <Formik>
+                        {
+                            (props) => (
+    
+                                <Form>
+                                    <fieldset className="form-group"> 
+                                        <label><strong>Username</strong></label>
+                                        <Field className="form-control" type="text" name="username"/>
+                                    </fieldset>
+                                    <br />
+                                    <fieldset className="form-group">
+                                        <label><strong>Password</strong></label>
+                                        <Field className="form-control" type="password" name="password"/>
+                                    </fieldset>
+                                    <br />
+                                    <button className="btn btn-success" type="submit">Login</button>
+                                </Form>
+                            )
+                        }
+                    </Formik> */}
+                </div>
+            </div>
         )
     }
 }
