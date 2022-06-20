c51591f72393731359293915b111a746e73f79a5
LiangkuLin <jerry031785@gmail.com>
2022-06-20 16:46:30 -0700

    add album to mainPage

--- src/components/HomeMainDisplay.jsx
+++ src/components/HomeMainDisplay.jsx
@@ -1,12 +1,96 @@
 import React, {Component} from "react";
+import axios from 'axios';
+
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useContext, useEffect } from 'react'
+
+
+
+
 
 class HomeMainDisplay extends Component{
-  
+    constructor(props){
+        super(props)
+        this.state = {
+         DogList:[]
+        }
+    }
+
+    async componentDidMount() {
+       await this.GetAllDogs();
+       console.log(this.state.DogList);
+    }
+
+     GetAllDogs = async() =>{
+        await axios.get('http://localhost:8080/getAllDogs')
+        .then((res)=>{this.setState({DogList:res.data})} )
+        .catch((e)=>console.log("Error: "+ e.message))
+    }
 
     render(){
+        const theme = createTheme();
         return(
-            <div>
-                <h1>STH JBBKBKNKN</h1>
+            <div>            
+                {/* <h1>STH JBBKBKNKN</h1>
+                <div className="mainDisplay">
+                    {this.state.DogList.map(dogname=>(
+                        <div key={dogname.id}><img src={decodeURIComponent(dogname.dogURL)}></img></div>
+                    ))}
+                </div> */}
+
+
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    
+                    <main>
+                        {/* Hero unit */}
+                    
+                        <Container sx={{ py: 4 }} maxWidth="lg">
+                        {/* End hero unit */}
+                        <Grid container spacing={4}>
+                            {this.state.DogList.map((dogs) => (
+                            <Grid item key={dogs} xs={12} sm={6} md={4} lg={3}>
+                                <Card
+                                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                >
+                                <CardMedia
+                                    component="img"
+                                    sx={{
+                                        height : 300
+                                    }}
+                                    image={decodeURIComponent(dogs.dogURL)}
+                                    alt="random"
+                                />
+                                <CardContent sx={{ flexGrow: 1 }}>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                    {dogs.dogname}
+                                    </Typography>
+                                    <Typography>
+                                    This is a media card. You can use this section to describe the
+                                    content.
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">View</Button>
+                                    <Button size="small">Edit</Button>
+                                </CardActions>
+                                </Card>
+                            </Grid>
+                            ))}
+                        </Grid>
+                        </Container>
+                    </main>
+                    </ThemeProvider>
+             
             </div>
         )
     }
