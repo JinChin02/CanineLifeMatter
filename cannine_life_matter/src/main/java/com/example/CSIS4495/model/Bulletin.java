54cc907d087819a8ea051fa1968f2a80d9d7618f
LiangkuLin <jerry031785@gmail.com>
2022-07-24 16:21:33 -0700

    commit for pushing

---
+++ cannine_life_matter/src/main/java/com/example/CSIS4495/model/Bulletin.java
@@ -0,0 +1,85 @@
+package com.example.CSIS4495.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Bulletin")
+public class Bulletin {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "description")
+	private String description;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name="owner", nullable = false)
+	private User owner;
+	
+	@Column(name = "date")
+	private String date;
+	
+	@Column(name = "time")
+	private String time;
+	
+	
+
+	public Bulletin() {
+		super();
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	
+	
+
+}
